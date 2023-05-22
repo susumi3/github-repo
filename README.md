0ac48432c54ad5d177260e3fe164d41ae8cac98c
susumi3 <133917467+susumi3@users.noreply.github.com>
2023-05-22 20:57:15 +0900

    Add files via upload

---
+++ README.md
@@ -0,0 +1,17 @@
+> Example
+
+```
+<repository>
+    <id>susumi3-repo</id>
+    <name>susumi3 repo</name>
+    <url>https://raw.github.com/susumi3/github-repo/main</url>
+</repository>
+```
+
+```
+<dependency>
+    <groupId>jp.susumi3</groupId>
+    <artifactId>Pan</artifactId>
+    <version>1.0.0</version>
+</dependency>
+```
---
+++ js/script.js
@@ -0,0 +1,62 @@
+'use script';
+
+const api_endpoint = "https://api.github.com/repos/susumi3/github-repo/contents";
+
+const path = location.pathname;
+
+$(document).ready(function() {
+    $.ajax({url: api_endpoint+path, dataType: 'json'})
+    .done(function(data) {
+        var ul = document.getElementById('file');
+
+        for (let i = 0; i < data.length; i++) {
+            const element = data[i];
+            const _name = element.name;
+            const _size = element.size;
+            const _type = element.type
+            const _download_url = element.download_url;
+            var ignore = ["js", "img", "css", "404.html", "README.md"]
+            if (path === "/" && ignore.includes(_name)) { continue; }
+            
+            var name = " ";
+            
+            var a = document.createElement('a');
+            var li = document.createElement('li');
+            var img = document.createElement('img');
+            if (_type === 'dir') {
+                name += _name + "/";
+                a.href = "/" + _name;
+                img.src = "img/folder.png";
+            }else {
+                var size;
+                if (_size === 0) {
+                      size = '0 Byte';
+                }
+                const k = 1024;
+                const sizes = ['Byte', 'KB', 'MB', 'GB', 'TB'];
+                const b = Math.floor(Math.log(_size) / Math.log(k));
+                size = (_size / Math.pow(k, b)).toFixed(2) + ' ' + sizes[b];
+
+                name += _name + " | size: " + size;
+                a.href = _download_url;
+                img.src = "img/file.png";
+            }
+            var text = document.createTextNode(name);
+
+            li.appendChild(img);
+            a.appendChild(text);
+            li.appendChild(a);
+            ul.appendChild(li);
+            
+        }
+    }).fail(function(err) {
+        const status = err.status
+        if (status == 429) {
+            window.alert("github api is retrun too may requests.")
+        }else if (status == 404) {
+            window.alert("github api is retrun file or folder not found.")
+        }else {
+            console.error(status);
+        }
+    });
+});
